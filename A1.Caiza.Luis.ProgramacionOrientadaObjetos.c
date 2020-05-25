ab4f0aaf0c248335daf04d791c2f8c68f963ea69
MateoNicolasAndrade <65574329+MateoNicolasAndrade@users.noreply.github.com>
2020-05-24 22:12:49 -0500

    Update A1.Caiza.Luis.ProgramacionOrientadaObjetos.c

--- A1.Caiza.Luis.ProgramacionOrientadaObjetos.c
+++ A1.Caiza.Luis.ProgramacionOrientadaObjetos.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+#define dim 50
 
-
-int main()
+int main ()
 {
-    printf("Hola mundo");
-    return 0;
-}
+    char ingreso [dim];
+    printf ("Hola Mundo\n");
+    printf ("Escriba algo: \n");
+    gets(ingreso);
+    printf ("%s",ingreso);
 
+    return(0);
+}
