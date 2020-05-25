6b40f83cf127a6a86210a47cd16ea2423911f89a
MateoNicolasAndrade <Mateo@DESKTOP-34D48NQ>
2020-05-24 21:55:42 -0500

    Prueba de Herramienta Github

---
+++ build/Debug/Cygwin-Windows/A1.Caiza.Luis.ProgramacionOrientadaObjetos.o.d
@@ -0,0 +1,2 @@
+build/Debug/Cygwin-Windows/A1.Caiza.Luis.ProgramacionOrientadaObjetos.o: \
+ A1.Caiza.Luis.ProgramacionOrientadaObjetos.c
---
+++ nbproject/Makefile-Release.mk
@@ -0,0 +1,84 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=Cygwin-Windows
+CND_DLIB_EXT=dll
+CND_CONF=Release
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/A1.Caiza.Luis.ProgramacionOrientadaObjetos.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/a1.caiza.luis.programacionorientadaobjetos.exe
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/a1.caiza.luis.programacionorientadaobjetos.exe: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/a1.caiza.luis.programacionorientadaobjetos ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/A1.Caiza.Luis.ProgramacionOrientadaObjetos.o: A1.Caiza.Luis.ProgramacionOrientadaObjetos.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/A1.Caiza.Luis.ProgramacionOrientadaObjetos.o A1.Caiza.Luis.ProgramacionOrientadaObjetos.c
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/a1.caiza.luis.programacionorientadaobjetos.exe
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
